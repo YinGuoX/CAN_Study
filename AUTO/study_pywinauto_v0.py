9e9bffc6b9421df5e65f69c01980405dd69a491a
YinGuoX <1339682021@qq.com>
2023-10-29 20:17:05 +0800

    add some pywinauto skills

--- AUTO/study_pywinauto_v0.py
+++ AUTO/study_pywinauto_v0.py
@@ -1,4 +1,6 @@
 # 官方文档代码
+import time
+
 from pywinauto.application import Application
 from pywinauto.findwindows import find_elements
 from  pywinauto.backend import registry
@@ -8,6 +10,10 @@ from  pywinauto.backend import registry
 # 连接客户端
 app = Application(backend="uia").connect(title_re="微信")
 print(app.windows())
+
+# 查看Windows系统下所有的窗口
+eles = find_elements(backend='uia')
+print(eles)
 
 # 2.元素架构
 # 获取对话框
@@ -74,3 +80,78 @@ for ele in eles:
     ele = registry.backends['uia'].generic_wrapper_class(ele)
     print(ele.texts())
     ele.draw_outline(colour='red')
+
+
+
+'''
+5. 元素的属性:但是怎么知道 元素是否有这些属性：使用代码
+    输入使用：set_text()
+    点击使用：click()、click_input()
+    获取使用：texts()
+
+ele = dlg.child_window(best_match='xx')
+print(dir(ele.element_info))
+'''
+ele = dlg.child_window(best_match='登录',control_type="Button")
+print(dir(ele.element_info))
+
+'''
+6. 显式等待与隐式等待
+    1. 隐式等待：自带，可以前后设置10倍
+    Timings.Fast()
+    2. 显式等待：自己实现，根据元素属性，实现
+    
+'''
+#
+# def wait_until_enabled(kwargs,wait_time=10,interval_time=0.5):
+#     """
+#     显式等待；等待元素状态是可用的
+#     :param kwargs: 元素定位参数
+#     :param wait_time:等待多久
+#     :param interval_time:隔多久去检验一次
+#     :return:
+#     """
+#     my_wait_time=0
+#     while my_wait_time<wait_time:
+#         time.sleep(interval_time)
+#         ele = dlg.child_window(**kwargs)
+#         print(dir(ele.element_info))
+#         print(ele.element_info.enabled)
+#         if ele.element_info.enabled:
+#             break
+#
+# wait_until_enabled({"best_match":'登录',"control_type":"Button"})
+
+'''
+7. 相对定位:先获取父元素，再获取子元素
+    获取父元素：father = dlg.child_window(best_math="xxx")
+    打印父元素下面的子元素的信息：father.print_control_identifiers()
+    获取子元素：father.child_window(best_math="xxx")
+    
+8. 菜单选择:分清楚：menu、menu_item
+    级联菜单选择：Menu
+        dlg.menu_select("File -> Export Session")
+        类型必须为：
+            control_type = "Menu"
+    子菜单的选择：Menu_Item
+        子菜单选择：ele.select()
+        子菜单类型：control_type = "MenuItem"
+
+eg:
+# 点击级联菜单
+dlg.menu_select("File -> Export Session")
+# 点击级联菜单后，点击里面的子菜单项
+menu_item = dlg.child_window(best_match = "All Sessions ...")
+menu_item.select()
+
+
+9. Combox(弹框)选项的选择
+    1. 法一：点出下拉框后再点击选项
+        dlg.child_window(best_match='ComboxBox').click_input()
+        dlg.child_window(best_match='菜单名').click_input()
+    2. 法二：dlg.child_window(best_match='ComboxBox').select('菜单名')
+    
+10. 文件对话框操作
+    pywinauto可以直接定位文件对话框！所以直接定位即可
+    dlg.child_window(title='文件名:'，control_type='Edit').set_text('测试路径')
+'''
